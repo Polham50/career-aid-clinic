8024a1782b1ab73cb240a16e9bb1573155869b8c
Your Name <you@example.com>
2025-09-06 21:14:20 +0100

    Initial commit / clean backend

--- dist-server/components/shared/Card.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const Card = ({ children, className = '', onClick }) => {
-    return ((0, jsx_runtime_1.jsx)("div", { onClick: onClick, className: `bg-white border border-gray-200 rounded-xl shadow-md ${className}`, children: children }));
-};
-exports.default = Card;
